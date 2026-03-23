d07e2f631d8567935f387ff21b0a2431997e7773
anurag <anurag.srivastava1902@gmail.com>
2026-03-24 00:00:21 +0530

    add manufacturer tests, pages

---
+++ pages/dashboard/manufacturer-dashboard.page.js
@@ -0,0 +1,37 @@
+export default class ManufacturerDashboardPage {
+    constructor(page) {
+        this.page = page;
+        this.dashboardBtn  = page.getByRole('button', { name: 'Dashboard' })
+        this.quotesToggle  = page.getByRole('button', { name: 'Quotes' })
+        this.quoteScreenBtn  = page.getByRole('button', { name: 'Quote Screen' })
+        this.quoteRepositoryBtn  = page.getByRole('button', { name: 'Quote Repository' })
+        this.managementConsoleBtn  = page.getByRole('button', { name: 'Management Console' })
+        this.userManagementBtn  = page.getByRole('button', { name: 'User Management' })
+    }
+
+    async goToDashboard(){
+        this.dashboardBtn.click();
+    }
+
+    async goToQuotes() {
+        
+    }
+
+    async goToQuoteScreen() {
+        await this.quotesToggle.click();
+        await this.quoteScreenBtn.click();
+    }
+
+    async goToQuoteRepository() {
+        await this.quotesToggle.click();
+        await this.quoteRepositoryBtn.click();
+    }
+
+    async goToManagementConsole() {
+        await this.managementConsoleBtn.click();
+    }
+
+    async goToUserManagement() {
+        await this.userManagementBtn.click();
+    }
+}
