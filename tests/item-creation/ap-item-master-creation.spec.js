11bda507d1b0f9b1ab57a2e9144933e8cafe5ad7
anurag <anurag.srivastava1902@gmail.com>
2026-03-31 12:10:22 +0530

    update ap-item-master test

--- tests/item-creation/ap-item-master-creation.spec.js
+++ tests/item-creation/ap-item-master-creation.spec.js
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
-import ApAdminDashboardPage from '../../pages/affordplan/dashboard/ap-admin-dashboard.page.js';
-import ApItemMasterFormPage from '../../pages/affordplan/affordplan-masters/ap-item-master-form.page.js';
-import ApItemMasterListPage from '../../pages/affordplan/affordplan-masters/ap-item-master-list.page.js';
 import { readCSV } from '../../utils/readCSV.js';
+import ApAdminMenu from '../../pages/affordplan/ap-admin-menu.page.js';
+import ApItemMasterListPage from '../../pages/affordplan/affordplan-masters/ap-item-master-list.page.js';
+import ApItemMasterFormPage from '../../pages/affordplan/affordplan-masters/ap-item-master-form.page.js';
 // import data from '../../test-data/dropdownMaster.json';
 // import getDropdownListFromApi from '../../utils/getDropdownListFromApi.js';
 
@@ -16,31 +16,23 @@ try {
 
 
 test.describe.parallel('testing item creation', () => {
-    test.use({ storageState: 'storage/auth.json' });
+    test.use({ storageState: 'storage/auth.ap_superadmin.json' });
     for (const apItem of apItems) {
         test(`Item creation tests -- ${apItem.itemName}`, async ({ page }) => {
-            const apAdminDashboardPage = new ApAdminDashboardPage(page);
+            const apAdminMenu = new ApAdminMenu(page);
             const apItemMasterListPage = new ApItemMasterListPage(page);
             const apItemMasterFormPage = new ApItemMasterFormPage(page);
 
             await page.goto("/dashboard");
-            await apAdminDashboardPage.goToApItemMaster();
+            await apAdminMenu.goToApItemMaster();
             await apItemMasterListPage.startNewAPItemCreation();
-
-            // const dropdownName = 'form'
-            // const result = await getDropdownListFromApi(page, `/api/v1/${dropdownName}s`, dropdownName)
-            // console.log(dropdownName);
-            // console.log(result);
-            // await page.pause();
-
-            await expect(page.getByText(/New Affordplan Item/)).toBeVisible();
+            await expect(apItemMasterFormPage.formHeading).toBeVisible();
             await apItemMasterFormPage.fillForm(apItem);
             await expect(page.getByText(apItem.itemName), `Expecting ${apItem.itemName} to be visible::\n`).toBeVisible({ timeout: 10000 });
             await expect(page.locator('.MuiSkeleton-root')).toHaveCount(0);
             await Promise.all([
                 page.waitForResponse(res => res.url().includes('api/v1/items')),
                 apItemMasterListPage.searchItem(apItem.itemName),
-                console.log("item searched....")
             ])
             await expect(page.locator('.MuiSkeleton-root')).toHaveCount(0);
             await expect(page.locator('table tbody tr', { hasText: apItem.itemName })).toBeVisible();
